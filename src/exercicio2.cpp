d3962624ba190403b573239b5ec2f256de5bceb7
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-09-01 10:14:41 -0300

    feat: exercicio 2

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 int main(void){
-
+    //a)
+    int a, b, c;
     
+    //b)
+    cout << "Digite dois números inteiros: ";
+    cin >> a >> b;
+
+    //c)
+    c = 4 * a + b / 3 - 5;
+
+    //d)
+    cout << "O resultado da expressão 4 * a + b / 3 - 5 é: " << c << endl;
+
+    //e)
+    c = (4 * (a + b)) / (3 - 5);
+    cout << "O resultado da expressão (4 * (a + b)) / (3 - 5) é: " << c << endl;
+
+    //f)
+    //O resultado das expressões são diferentes pois a precedência dos operadores é diferente.
+    // na primeira expressão, como nao usa parenteses, a precedência é: 4*a + (b/3) - 5.
+    // na segunda expressão usa parenteses por tanto a precedencia segue o indicado.
+
+    //g)
+    c = (a*a) + 2*b + c;
+    cout << "O resultado da expressão a² + 2 * b + c é: " << c << endl;
+
     return 0;
 }
