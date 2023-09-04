04ce6dcf4357cef4a53e1a79c66bcbb4179c9dc6
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-04 09:12:13 -0300

    feat: exercicio 2f

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -5,16 +5,34 @@ using namespace std;
 int main(void){
     int a, b, c;
 
-    cout << "Digite um inteiro:";
+    cout << "Digite um inteiro: ";
     cin >> a;
-    cout << "Digite um inteiro:";
+    cout << "Digite um inteiro: ";
     cin >> b;
 
+    /*  como a multiplicação e divisao tem precedencia
+        4a e b/3 sao executados primeiro, depois eles
+        somados e o resultaedo é diminuido em 5 unidades
+    */ 
     c = 4*a + b/3 - 5;
     cout << "4*a + b/3 - 5 = " << c << endl;
 
+    /*  como o q esta entre aparenteses tem priooridade, 
+        (a + b) e (3 - 5) sao executados primeiro, esse resultado
+        é usado pra fazer a divisao e a multiplicacao
+    */ 
     c = 4*(a + b)/(3 - 5);
     cout << "4*(a + b)/(3 - 5) = " << c << endl;
+
+    // outras espressões que geram resultados diferentes
+    c = 4*(a + (b/3) - 5);
+    cout << "4*(a + (b/3) - 5) = " << c << endl;
+    c = 4*(a + (b/3)) - 5;
+    cout << "(4*a) + (b/3) - 5 = " << c << endl;
+    c = 4*(a + b)/3 - 5;
+    cout << "4*a + (b/3 - 5) = " << c << endl;
+
+
 
     
     
