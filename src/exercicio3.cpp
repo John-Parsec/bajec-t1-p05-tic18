bd88f71b9068776a52b18b0897282faa9507a287
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-04 10:45:30 -0300

    feat: exercicio 3e

--- src/exercicio3.cpp
+++ src/exercicio3.cpp
@@ -15,8 +15,12 @@ int main(void){
     cout << "(dec)" << (int)ch3  << " | (oct)" << oct << (int)ch3 << " | (hex)" << hex << (int)ch3;
     cout << dec << endl;
 
+
     ch3 = ch2 -1;
     printf("Anteccessor de %c(%d): (dec)%d | (oct)%o | (hex)%X\n", ch2, ch2, ch3, ch3, ch3);
+
+    ch3 = isupper(ch1)? 'A' : ' ';
+    cout << "ch1 = " << ch1 <<" | ch3 = " << ch3 << endl;
 
     
     return 0;
