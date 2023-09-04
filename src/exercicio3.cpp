d64b002ae513011633cddb0c34b9f7112854bbaf
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-04 10:15:36 -0300

    feat: exercicios 3a, 3b, 3c

--- src/exercicio3.cpp
+++ src/exercicio3.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(void){
+    char ch1, ch2, ch3;
+
+    cout << "Digite um caractere: ";
+    cin  >> ch1;
+    cout << "Digite um caractere: ";
+    cin  >> ch2;
+
+    ch3 = ch1 -1;
+    cout << "Anteccessor de " << ch1 << "(" << (int)ch1<<  ")" <<": ";
+    cout << "(dec)" << (int)ch3  << " | (oct)" << oct << (int)ch3 << " | (hex)" << hex << (int)ch3;
+    cout << dec << endl;
 
     
     return 0;
