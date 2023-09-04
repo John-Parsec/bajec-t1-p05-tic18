d656dd40f45bfb00b10bc1b34b6f5a22192f0d3d
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-04 08:43:13 -0300

    feat: exercicio 2a, 2b, 2c, 2d, 2e

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main(void){
+    int a, b, c;
 
+    cout << "Digite um inteiro:";
+    cin >> a;
+    cout << "Digite um inteiro:";
+    cin >> b;
+
+    c = 4*a + b/3 - 5;
+    cout << "4*a + b/3 - 5 = " << c << endl;
+
+    c = 4*(a + b)/(3 - 5);
+    cout << "4*(a + b)/(3 - 5) = " << c << endl;
+
+    
     
     return 0;
 }
