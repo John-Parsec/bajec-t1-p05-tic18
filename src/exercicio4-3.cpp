0f334131f51ff5f792ac773de7ca24ef5c7a58ec
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-05 18:03:27 -0300

    feat: exercicio 4-3

--- src/exercicio4-3.cpp
+++ src/exercicio4-3.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main(void){
+    double x, y;
 
+    cout << "Digite a coordena X: ";
+    cin >> x;
+    cout << "Digite a coordena Y: ";
+    cin >> y;
+
+    cout << ((x == 0 || y == 0)? "Ponto sobre eixo": 
+            ((x > 0 && y > 0)? "Ponto no primeiro quadrante": 
+            ((x > 0)? "Ponto no quarto quadrante": 
+            ((y > 0)? "Ponto no segundo quadrante": "Ponto no terceiro quadrante"))));
     
     return 0;
 }
