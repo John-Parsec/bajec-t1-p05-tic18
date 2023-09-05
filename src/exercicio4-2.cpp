73190282590708aa6a3d6711f43cd4c25b2744a4
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-05 17:56:03 -0300

    feat: exercicio 4-2

--- src/exercicio4-2.cpp
+++ src/exercicio4-2.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(void){
+    double raio;
 
+    cout << "Digite o raio: ";
+    cin >> raio;
     
+    cout << "Diametro: " << 2*raio << endl;
+    cout << "Circunferencia: " << 2*M_PI*raio << endl;
+    cout << "Area: " << M_PI*raio*raio << endl;
+
     return 0;
 }
