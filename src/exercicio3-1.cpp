63dc1977e4d249b96d2e6a63020eec9359c95648
João Manoel <jmaoliveira.cic@uesc.br>
2023-09-05 22:25:22 -0300

    fix: exercicio 3

--- src/exercicio3-1.cpp
+++ src/exercicio3-1.cpp
@@ -2,30 +2,6 @@
 
 using namespace std;
 
-/*
-Escreva um programa que
-c. Atribua à variável ch3 o caractere que antecede ao caractere ch1,
-imprima na tela, utilizando cout, o caractere em formato numérico 
-decimal, octal, hexadecimal e como caractere; (caso ch3 não seja um
-caractere imprimível, substitua ele pelo caractere '_')
-d. Atribua à variável ch3 o caractere que precede ao caractere ch2,
-imprima na tela, utilizando printf, o caractere em formato numérico
-decimal, octal, hexadecimal e como caractere; (caso ch3 não seja um
-caractere imprimível, substitua ele pelo caractere '_')
-e. Atribua à variável ch3 o valor 'A' se o caractere ch1 for uma letra
-maiúscula e o valor ' '(caractere espaço vazio) caso contrário. Use o
-operador condicional (?) com esta finalidade. Imprima o valor de ch3
-na tela;
-f. Atribua à variável ch3 o valor 'a' se o caractere ch2 for uma letra
-minúscula e o valor ' '(caractere espaço vazio) caso contrário. Use o
-operador condicional (?) com esta finalidade. Imprima o valor de ch3
-na tela;
-g. Atribua à variável ch3 o valor '1' se o caractere ch1 ou o caractere
-ch2 forem um dígito e o valor ' '(caractere espaço vazio) caso
-contrário. Use o operador condicional (?) com esta finalidade.
-Imprima o valor de ch3 na tela;
-*/
-
 int main(void){
     //a)
     char ch1, ch2, ch3;
@@ -38,13 +14,13 @@ int main(void){
     ch3 = ch1 - 1;
     cout << "O caractere que antecede o caractere " << ch1 << " é: " << ch3 << endl;
     cout << "\tDec\tHex\tOct\n";
-    cout << "'" << ch3 <<"' -\t" << dec << (int)ch3 << '\t' << hex << (int)ch3 << '\t' << oct << (int)ch3 << '\t' << '\n';
+    ch3 < 32 || ch3 > 126 ? cout << "'" << "_" <<"' -\t" << dec << (int)ch3 << '\t' << hex << (int)ch3 << '\t' << oct << (int)ch3 << '\t' << '\n' : cout << "'" << ch3 <<"' -\t" << dec << (int)ch3 << '\t' << hex << (int)ch3 << '\t' << oct << (int)ch3 << '\t' << '\n';
 
     //d)
     ch3 = ch2 - 1;
     printf("O caractere que precede o caractere %c é: %c\n", ch2, ch3);
     printf("\tDec\tHex\tOct\n");
-    printf("'%c' -\t%d\t%x\t%o\t\n", ch3, ch3, ch3, ch3);
+    ch3 < 32 || ch3 > 126 ? printf("_ -\t%d\t%x\t%o\t\n", ch3, ch3, ch3) : printf("'%c' -\t%d\t%x\t%o\t\n", ch3, ch3, ch3, ch3);
 
     //e)
     ch3 = (ch1 >= 'A' && ch1 <= 'Z') ? 'A' : ' ';
