adb814ac305edd6765870ab8936e853dcf853baf
João Manoel <jmaoliveira.cic@uesc.br>
2023-09-05 22:27:54 -0300

    fix: exercicio 3-2

--- src/exercicio3-2.cpp
+++ src/exercicio3-2.cpp
@@ -2,20 +2,9 @@
 
 using namespace std;
 
-/*
-Escreva um programa que:
-peça ao usuário para fornecer dois números inteiros, obtenha-os da
-entrada padrão, usando cin, e imprima o maior deles seguido das
-palavras “e maior”.
-b. Se os números foram iguais, imprima a mensagem “estes
-números são iguais”. Use apenas o operador condicional (?).
-c. Identifique se o maior valor é um número par ou ímpar.
-*/
-
 int main(void){
     //a)
     int n1, n2, maior;
-
 
     cout << "Digite dois numeros inteiros: ";
     cin >> n1 >> n2;
